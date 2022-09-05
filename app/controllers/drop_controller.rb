2391e8139e3b860156e0181ac5ca1c009a651f71
Saif-041 <sp19-bse-041@cuilahore.edu.pk>
2022-09-05 19:38:14 +0500

    Squashed commit of the following:
    commit 65108617454fefd786c28b9f29505339d2233d97
    Author: usmanasif2207 <usmanasif@crosshairtechnology.com>
    Date:   Mon Sep 5 19:34:23 2022 +0500
    
        added cities functionalities

--- app/controllers/drop_controller.rb
+++ app/controllers/drop_controller.rb
@@ -1,4 +1,5 @@
 class DropController < ApplicationController
+
     def show
         @user = User.find_by_id(params[:id])
         if @user.nil?
@@ -20,6 +21,7 @@ class DropController < ApplicationController
             end
         end
     end
+
     def collect
         @user = User.find_by_id(params[:player_id])
         @drop = Drop.find_by_id(params[:drop_id])
@@ -40,10 +42,11 @@ class DropController < ApplicationController
                     status: 401
                 }, status: :ok
             elsif @drop.is_active == true
-                @drop.is_active = false
-                @drop.save
                 @user.total_collection = @user.total_collection + @drop.reward_amount
                 @user.save
+                @drop.is_active = false
+                @drop.reward_amount = 0.0
+                @drop.save
                 render json: {
                     message: "Drop Collected!  #{@drop.reward_amount}$ added to User id: #{@user.id}",
                     status: 200
@@ -56,4 +59,37 @@ class DropController < ApplicationController
             end
         end
     end
+
+    def city
+      @re = []
+      # @citi = CS.states(params[:country]).keys.flat_map {
+      #     |state| @re.concat(CS.cities(state, params[:country]))
+      # }
+      @prov = CS.states(params[:country]).map {
+          |k,state|
+          if params[:province] == state
+              @re=k
+          end
+      }
+              # render json: { data: @re.sort! }, status: :ok
+      @cities = CS.cities(@re, params[:country])
+      render json: { data: @cities }, status: :ok
+  end
+  
+  def province
+      @provinces = []
+      @states = CS.states(params[:country]).map {
+          |k,state| @provinces.append(state)
+      }
+      render json: { data: @provinces }, status: :ok
+      # @provinces = CS.states(params[:country])
+      #     render json: { data: @provinces }, status: :ok
+  end
+
+    def import
+        Drop.import(params[:file])
+        redirect_to admin_drops_path, notice: "drops added"
+    end
+
+
 end

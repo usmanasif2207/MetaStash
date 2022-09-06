0965906d7d51f691d6b257a74b6df58d50416d77
USMAN ASIF <usmanasif@crosshairtechnology.com>
2022-09-06 19:25:59 +0500

    Update 20220906142113_m1.rb

--- db/migrate/20220906142113_m1.rb
+++ db/migrate/20220906142113_m1.rb
@@ -1,5 +1,5 @@
 class M1 < ActiveRecord::Migration[7.0]
   def change
-    add_column :feedbacks, :is_resolved , default: false
+    add_column :feedbacks, :is_resolved,:boolean , default: false
   end
 end

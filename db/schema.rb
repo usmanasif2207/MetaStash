0ffa0704415c15fdf20e94431be7de362b5a8739
USMAN ASIF <usmanasif@crosshairtechnology.com>
2022-09-26 11:58:38 +0500

    Rake Task Updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_115346) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_142113) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,11 +72,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_115346) do
     t.string "reward_type", default: "Dollar"
     t.float "reward_amount", default: 0.0
     t.boolean "is_active", default: false
+    t.float "longitude"
+    t.float "latitude"
     t.datetime "time_to_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "longitude"
-    t.float "latitude"
     t.string "city"
     t.string "country"
     t.string "province"
--- lib/tasks/polls.rake
+++ lib/tasks/polls.rake
@@ -1,11 +1,12 @@
 namespace :polls do
   desc "TODO"
   task delete_30_days_old: :environment do
-    @poll = Poll.where(['created_at < ?', 30.days.ago])
-    if @poll[0].is_active == true
-        @poll[0].is_active = false
-        @poll[0].save
+   Poll.where(['created_at < ?', 30.days.ago]).each do |pol| 
+      if pol.is_active == true
+        pol.is_active = false
+        pol.save
         puts "30 Days old Poll is expired"
+      end
     end
   end
 end

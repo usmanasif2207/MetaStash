70096aa4d62d47562c9f8a9b83f07395a5dbdc84
USMAN ASIF <usmanasif@crosshairtechnology.com>
2022-09-26 09:47:37 +0500

    Minor Changes

--- db/schema.rb
+++ db/schema.rb
@@ -72,7 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_115346) do
     t.string "reward_type", default: "Dollar"
     t.float "reward_amount", default: 0.0
     t.boolean "is_active", default: false
-    t.float "longitude"
+    t.float "is_active"
     t.float "latitude"
     t.datetime "time_to_active"
     t.datetime "created_at", null: false

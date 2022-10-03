dcec1f83ee4466a515d1be9921239e9ff119df16
USMAN ASIF <usmanasif@crosshairtechnology.com>
2022-10-03 16:31:39 +0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-    
-    protect_from_forgery with: :null_session
+    before_action :authenticate_user!
+    protect_from_forgery prepend: true
 end

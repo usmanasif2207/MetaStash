c1eccd5fb559d468bd5710a6dcd17e37b4735112
USMAN ASIF <usmanasif@crosshairtechnology.com>
2022-10-03 14:48:38 +0500

    Update api_controller.rb

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,4 @@
 class ApiController < ActionController::API
-    skip_before_action :verify_authenticity_token
     before_action :configure_permitted_parameters, if: :devise_controller?
  
   protected

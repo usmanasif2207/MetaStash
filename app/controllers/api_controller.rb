223b9e7a478f9dbfdaf7c604c5819f936f6cc0f3
USMAN ASIF <usmanasif@crosshairtechnology.com>
2022-10-06 12:45:04 +0500

    Deleted Api Controller

--- app/controllers/api_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApiController < ActionController::API
-    before_action :configure_permitted_parameters, if: :devise_controller?
- 
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:city,:country])
-  end
-end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-        protect_from_forgery with: :null_session
+    protect_from_forgery with: :exception, if: Proc.new { |c| c.request.format != 'application/json' }
+    protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+    
+    before_action :configure_permitted_parameters, if: :devise_controller?
+ 
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:city,:country])
+  end
 end
